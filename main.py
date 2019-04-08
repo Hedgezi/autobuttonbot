588686c4e2c75b1e710ef5f6191494e8c5541c91
Kolya <25066027+Hedgezi@users.noreply.github.com>
2019-04-08 21:43:00 +0300

    main + autosetup
    добавил установщик нужных модулей (отдельным скриптом) и добавил рандомный разброс секунд в скрипте

--- main.py
+++ main.py
@@ -10,13 +10,16 @@ commandsstorage = []
 startstop = False
 
 def addcommand():
-	global addbutton, commandslb, secondsrepeatble, commandsrepeatble, secondshold
-	srptext, crptext, shdtext = secondsrepeatble.get(), commandsrepeatble.get(), secondshold.get()
+	global addbutton, commandslb, secondsrepeatble, commandsrepeatble, secondshold, randspread
+	srptext, crptext, shdtext, rndtext = secondsrepeatble.get(), commandsrepeatble.get(), secondshold.get(), randspread.get()
 	secondsrepeatble.delete(0,END)
 	commandsrepeatble.delete(0,END)
 	secondshold.delete(0,END)
-	commandsstorage.append([srptext, crptext, shdtext])
-	commandslb.insert(END, 'Каждые {0} секунд выполняется команда {1} с зажимом в {2} сек.'.format(srptext, crptext, shdtext))
+	randspread.delete(0,END)
+	secondshold.insert(0,'1')
+	randspread.insert(0,'2')
+	commandsstorage.append([srptext, crptext, shdtext, rndtext])
+	commandslb.insert(END, 'Каждые {0} секунд выполняется команда {1} с зажатием в {2} сек. и рандомным разбросом в {3} сек.'.format(srptext, crptext, shdtext, rndtext))
 
 def deletecommand():
 	global commandslb
@@ -27,21 +30,21 @@ def startrepcmd():
 	global commandsstorage, startstop
 	startstop = True
 	for i in commandsstorage:
-		repeatcommands(i[0], i[1], i[2])
+		repeatcommands(i[0], i[1], i[2], i[3])
 
 def stoprepcmd():
 	global startstop
 	startstop = False
 
 @multitasking.task
-def repeatcommands(secs, buttons, holdsecs):
+def repeatcommands(secs, buttons, holdsecs, randsecs):
 	global startstop
-	secs, holdsecs = int(secs), int(holdsecs)
+	secs, holdsecs, randsecs = int(secs), int(holdsecs), int(randsecs)
 	while startstop:
 		presskey(buttons, True, False)
 		time.sleep(holdsecs)
 		presskey(buttons, False, True)
-		time.sleep(random.randint(secs - 2, secs + 2))
+		time.sleep(random.randint(secs - randsecs, secs + randsecs))
 
 root = Tk()
 root.title("Кнопкобот")
@@ -49,25 +52,29 @@ root.resizable(False, False)
 
 secondsrepeatble = Entry(root, width=5)
 commandsrepeatble = Entry(root, width=15)
-commandslb = Listbox(root, height=5, width=70, selectmode=SINGLE)
+commandslb = Listbox(root, height=5, width=95, selectmode=SINGLE)
 addbutton = Button(root, text='Добавить команду', command=addcommand)
 deletebutton = Button(root, text='Удалить команду', command=deletecommand)
 startrepeating = Button(root, text='Запустить бота', command=startrepcmd)
 stoprepeating = Button(root, text='Остановить бота', command=stoprepcmd)
 secondshold = Entry(root, width=3)
+randspread = Entry(root, width=3)
 
 secondshold.insert(END,'1')
+randspread.insert(END,'2')
 
 Label(root, text='Каждые ').grid(row=0, column=0)
 secondsrepeatble.grid(row=0, column=1)
 Label(root, text=' секунд, повторять команды кнопок:').grid(row=0, column=2)
 commandsrepeatble.grid(row=0, column=3)
-Label(root, text='-').grid(row=0, column=4)
+Label(root, text='Зажатие -').grid(row=0, column=4)
 secondshold.grid(row=0, column=5, padx=5)
-addbutton.grid(row=0, column=6)
-deletebutton.grid(row=1, column=6)
+Label(root, text='Ранд.раз. -').grid(row=0, column=6)
+randspread.grid(row=0, column=7, padx=5)
+addbutton.grid(row=0, column=8)
+deletebutton.grid(row=1, column=8)
 startrepeating.grid(row=2, column=2)
 stoprepeating.grid(row=2, column=3)
-commandslb.grid(row=1, column=0, columnspan=6)
+commandslb.grid(row=1, column=0, columnspan=8)
 
 root.mainloop()
---
+++ setupmodules.py
@@ -0,0 +1,5 @@
+import os
+
+os.system('python -m pip install --upgrade pip')
+os.system('pip install keyboard')
+os.system('pip install multitasking')
